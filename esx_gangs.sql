40d7956773ebc586557004418e4a9865ac460f0c
crowmedic <jkbtrains@msn.com>
2017-11-20 18:41:12 -0500

    Update esx_gangs.sql

--- esx_gangs.sql
+++ esx_gangs.sql
@@ -1,1 +1,22 @@
+USE `essentialmode`;
+
+CREATE TABLE 'gangs' (
+
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE 'gang_grades' (
+
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gang_name` varchar(255) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
 
